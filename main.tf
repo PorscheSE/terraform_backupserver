5a0ed7afe8739744752677b31ff4efcfd030f2c1
Porsche Rodjanasak <45418798+PorscheSE@users.noreply.github.com>
2020-04-07 16:24:05 +0700

    update

--- main.tf
+++ main.tf
@@ -2,58 +2,58 @@
 provider "azurerm" {
     subscription_id = "8afbe872-4126-415f-bbf5-59890b64e029"
     client_id       = "7838c180-37d1-4ef8-a13b-b872a00d5c96"
-    client_secret   = "1R3a43RH=hR=@A[H4_3I?xpQIjB9.6y="
+    client_secret   = var.secret
     tenant_id       = "6e06e42d-6925-47c6-b9e7-9581c7ca302a"
     features {}
 }
 
 # Create a resource group if it doesn’t exist
-resource "azurerm_resource_group" "myterraformgroup" {
-    name     = "${var.prefix}-ResourceGroup"
-    location = "eastus"
+resource "azurerm_resource_group" "backuprg" {
+    name     = var.prefix-ResourceGroup
+    location = var.location
 
     tags = {
-        environment = "Terraform Demo"
+        environment = var.environment
     }
 }
 
 # Create virtual network
-resource "azurerm_virtual_network" "myterraformnetwork" {
-    name                = "myVnet"
+resource "azurerm_virtual_network" "backupNetwork" {
+    name                = "var.prefix-Vnet"
     address_space       = ["10.0.0.0/16"]
-    location            = "eastus"
-    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+    location            = var.location
+    resource_group_name = azurerm_resource_group.backuprg.name
 
     tags = {
-        environment = "Terraform Demo"
+       environment = var.environment
     }
 }
 
 # Create subnet
-resource "azurerm_subnet" "myterraformsubnet" {
-    name                 = "mySubnet"
-    resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
-    virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
+resource "azurerm_subnet" "backupSubnet" {
+    name                 = "var.prefix-Subnet"
+    resource_group_name  = azurerm_resource_group.backuprg.name
+    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
     address_prefix       = "10.0.1.0/24"
 }
 
 # Create public IPs
-resource "azurerm_public_ip" "myterraformpublicip" {
-    name                         = "myPublicIP"
-    location                     = "eastus"
-    resource_group_name          = "${azurerm_resource_group.myterraformgroup.name}"
+resource "azurerm_public_ip" "backupPublicIP" {
+    name                         = "var.prefix-PublicIP"
+    location 					 = var.location
+    resource_group_name          = azurerm_resource_group.backuprg.name
     allocation_method            = "Dynamic"
 
     tags = {
-        environment = "Terraform Demo"
+        environment = var.environment
     }
 }
 
 # Create Network Security Group and rule
-resource "azurerm_network_security_group" "myterraformnsg" {
-    name                = "myNetworkSecurityGroup"
-    location            = "eastus"
-    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+resource "azurerm_network_security_group" "backupNSG" {
+    name                = "var.prefix-NetworkSecurityGroup"
+    location            = var.location
+    resource_group_name = azurerm_resource_group.backuprg.name
     
     security_rule {
         name                       = "SSH"
@@ -68,26 +68,26 @@ resource "azurerm_network_security_group" "myterraformnsg" {
     }
 
     tags = {
-        environment = "Terraform Demo"
+        environment = var.environment
     }
 }
 
 # Create network interface
-resource "azurerm_network_interface" "myterraformnic" {
-    name                      = "myNIC"
-    location                  = "eastus"
-    resource_group_name       = "${azurerm_resource_group.myterraformgroup.name}"
-    network_security_group_id = "${azurerm_network_security_group.myterraformnsg.id}"
+resource "azurerm_network_interface" "backupNIC" {
+    name                      = "var.prefix-NIC"
+    location                  = var.location
+    resource_group_name       = azurerm_resource_group.backuprg.name
+    network_security_group_id = azurerm_network_security_group.myterraformnsg.id
 
     ip_configuration {
-        name                          = "myNicConfiguration"
-        subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
+        name                          = "var.prefix-NicConfiguration"
+        subnet_id                     = azurerm_subnet.myterraformsubnet.id
         private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"
+        public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
     }
 
     tags = {
-        environment = "Terraform Demo"
+        environment = var.environment
     }
 }
 
@@ -95,51 +95,51 @@ resource "azurerm_network_interface" "myterraformnic" {
 resource "random_id" "randomId" {
     keepers = {
         # Generate a new ID only when a new resource group is defined
-        resource_group = "${azurerm_resource_group.myterraformgroup.name}"
+        resource_group = azurerm_resource_group.backuprg.name
     }
     
     byte_length = 8
 }
 
 # Create storage account for boot diagnostics
-resource "azurerm_storage_account" "mystorageaccount" {
+resource "azurerm_storage_account" "backupStorageAccount" {
     name                        = "diag${random_id.randomId.hex}"
-    resource_group_name         = "${azurerm_resource_group.myterraformgroup.name}"
-    location                    = "eastus"
+    resource_group_name         = azurerm_resource_group.backuprg.name
+    location                    = var.location
     account_tier                = "Standard"
     account_replication_type    = "LRS"
 
     tags = {
-        environment = "Terraform Demo"
+        environment = var.environment
     }
 }
 
 # Create virtual machine
-resource "azurerm_virtual_machine" "myterraformvm" {
-    name                  = "myVM"
-    location              = "eastus"
-    resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
+resource "azurerm_virtual_machine" "backupVM" {
+    name                  = var.prefix-Sever
+    location              = var.location
+    resource_group_name   = azurerm_resource_group.backuprg.name
     network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
-    vm_size               = "Standard_DS1_v2"
+    vm_size               = "Standard_B1s"
 
     storage_os_disk {
-        name              = "myOsDisk"
+        name              = var.prefix-OsDisk
         caching           = "ReadWrite"
         create_option     = "FromImage"
         managed_disk_type = "Premium_LRS"
     }
 
     storage_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "16.04.0-LTS"
+        publisher = "RedHat"
+        offer     = "RHEL"
+        sku       = "7-RAW"
         version   = "latest"
     }
 
     os_profile {
-        computer_name  = "myvm"
-        admin_username = "azureuser"
-        admin_password = "P@ssw0rd1234!2"
+        computer_name  = var.prefix-Sever
+        admin_username = "admin"
+        admin_password = var.dbpassword
     }
 
     os_profile_linux_config {
@@ -148,10 +148,10 @@ resource "azurerm_virtual_machine" "myterraformvm" {
 
     boot_diagnostics {
         enabled = "true"
-        storage_uri = "${azurerm_storage_account.mystorageaccount.primary_blob_endpoint}"
+        storage_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
     }
 
     tags = {
-        environment = "Terraform Demo"
+        environment = var.environment
     }
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,20 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
+  default     = "backup"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "environment" {
+  description = "environment tag."
+}
+
+variable "secret" {
+  description = "secret AD password."
+}
+
+variable "dbpassword" {
+  description = "database password."
 }
